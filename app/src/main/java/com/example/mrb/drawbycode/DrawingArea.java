f217ed2cd2c08e14ffbc76b055cc4f61339b320e
mat_688@yahoo.ca <uncharted3>
2016-05-03 14:18:30 -0500

    The code works and makes a stick figure, a grid, and a checkerboard pattern

--- app/src/main/java/com/example/mrb/drawbycode/DrawingArea.java
+++ app/src/main/java/com/example/mrb/drawbycode/DrawingArea.java
@@ -16,7 +16,7 @@ public class DrawingArea extends View // You need to extend the View class
 
     Paint pntTextPaint;     // Painting object to handle the paint jobs for graphics
     Paint pntGraphicsPaint; // Painting object to handle the paint jobs for text
-
+    Paint pntOtherPaint;
     boolean blnShowText;
     int intTextPosition;
 
@@ -46,9 +46,16 @@ public class DrawingArea extends View // You need to extend the View class
 
         pntTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         pntGraphicsPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        pntOtherPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-        pntGraphicsPaint.setColor(Color.BLACK);
+        pntGraphicsPaint.setColor(Color.BLUE);
         pntGraphicsPaint.setStrokeWidth(3);
+
+        pntTextPaint.setColor(Color.GREEN);
+        pntTextPaint.setStrokeWidth(3);
+
+        pntOtherPaint.setColor(Color.BLACK);
+        pntOtherPaint.setStrokeWidth(3);
 
     }
 
@@ -68,11 +75,50 @@ public class DrawingArea extends View // You need to extend the View class
         // The Canvas parameter is what we draw ON; the Paint objects defined above are what we draw WITH.
 
         super.onDraw(canvas);
+        /*
+        canvas.drawCircle((float)325.0,(float)100.0,(float)100.0,pntGraphicsPaint);
 
-        canvas.drawCircle((float)50.0, (float)50.0, (float)50.0, pntGraphicsPaint);
+        canvas.drawRect((float)300.0, (float)125.0,(float)350.0,(float)500.0, pntGraphicsPaint);
 
+        canvas.drawRect((float)150.0,(float)250.0,(float)500.0,(float)300.0,pntGraphicsPaint);
 
+        canvas.drawRect((float)400.0,(float)450.0,(float)250.0,(float)500.0,pntGraphicsPaint);
+
+        canvas.drawRect((float)200.0,(float)450.0,(float)250.0,(float)700.0,pntGraphicsPaint);
+        canvas.drawRect((float)400.0,(float)450.0,(float)450.0,(float)700.0,pntGraphicsPaint);
+
+        canvas.drawCircle((float)280.0, (float) 80.0, (float)20.0,pntOtherPaint);
+        canvas.drawCircle((float)370.0, (float) 80.0,(float) 20.0,pntOtherPaint);
+
+       for(int i = 0; i<canvas.getWidth() ; i+= lineStart) {
+            canvas.drawLine((float)i,(float)0.00,(float)i,(float)canvas.getHeight(),pntOtherPaint);
+       }
+        for(int i = 0; i <canvas.getHeight(); i+= lineStart) {
+            canvas.drawLine((float) 0.00, (float) i, (float) canvas.getWidth(), (float) i, pntOtherPaint);
+        }
+        **/
+        boolean isBlue = true;
+        float leftSide = (float) 0.0;
+        float rightSide = (float) 100.0;
+        float TopSide = (float) 100.00;
+        float botLength = (float) 0.0;
+       do{
+           if(isBlue){
+               pntGraphicsPaint.setColor(Color.BLUE);
+               isBlue = false;
+           }else{
+               pntGraphicsPaint.setColor(Color.RED);
+               isBlue = true;
+           }
+           canvas.drawRect(leftSide, botLength, rightSide, TopSide, pntGraphicsPaint);
+           leftSide += 100.0;
+           rightSide += 100.0;
+           if(rightSide >= canvas.getWidth()){
+               botLength += 100.0;
+               TopSide += 100.0;
+               leftSide = (float) 0.0;
+               rightSide = (float) 100.0;
+           }
+       }while(canvas.getHeight() >= botLength);
     }
-
-
 }
